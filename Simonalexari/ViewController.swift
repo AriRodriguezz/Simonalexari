f8859afa7bc9459753bef8e66d3be5cf55e9015e
ALEJANDRO MORA <rl200042@rlas-macair-35.rlas-116.org>
2018-09-19 12:14:30 -0500

    Initial Commit

---
+++ Simonalexari/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simonalexari
+//
+//  Created by ALEJANDRO MORA on 9/19/18.
+//  Copyright © 2018 ALEJANDRO MORA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
